2ab26de57baadf6eebf75641b6c7f473d30f5f98
yogithesymbian <yogirenbox33@gmail.com>
2019-12-11 10:27:34 +0800

    error instrument test

--- app/src/androidTest/java/id/scode/kadeooredoo/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/id/scode/kadeooredoo/ui/home/HomeActivityTest.kt
@@ -1,7 +1,16 @@
 package id.scode.kadeooredoo.ui.home
 
-import org.junit.Before
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import id.scode.kadeooredoo.R.id.*
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * @Authors scode | Yogi Arif Widodo
@@ -14,13 +23,49 @@ import org.junit.Test
  * JVM: OpenJDK 64-Bit Server VM by JetBrains s.r.o
  * Linux 5.2.0-kali3-amd64
  */
+@RunWith(AndroidJUnit4::class)
 class HomeActivityTest {
-
-    @Before
-    fun setUp() {
-    }
+    @Rule
+    @JvmField var activityRule: ActivityTestRule<HomeActivity> = ActivityTestRule(HomeActivity::class.java)
 
     @Test
-    fun onCreate() {
+    fun testAppBehaviour() {
+
+        onView(withId(spinner))
+            .check(matches(isDisplayed()))
+        onView(withId(spinner)).perform(click())
+        onView(withText("Spanish La Liga")).perform(click())
+
+        onView(withText("Barcelona"))
+            .check(matches(isDisplayed()))
+        onView(withText("Barcelona")).perform(click())
+
+        onView(withId(add_to_favorite))
+            .check(matches(isDisplayed()))
+        onView(withId(add_to_favorite)).perform(click())
+        onView(withText("Added to favorite"))
+            .check(matches(isDisplayed()))
+        pressBack()
+
+        onView(withId(bottom_navigation))
+            .check(matches(isDisplayed()))
+
+        onView(withId(btm_fav)).perform(click())
     }
+//    @Test
+//    fun testRecyclerViewBehaviour() {
+//
+//        onView(withId(rv_list_team))
+//            .check(matches(isDisplayed()))
+//
+//        onView(withId(rv_list_team)).perform(
+//            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+//                10
+//            )
+//        )
+//
+//        onView(withId(rv_list_team)).perform(
+//            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(10, click())
+//        )
+//    }
 }
